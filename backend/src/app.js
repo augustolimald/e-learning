585aabcfd3a84249d1347a4dcf5b52c456e127e5
Augusto Lima <augustocesar827@gmail.com>
2019-10-11 19:09:12 -0300

    Adicionado CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 
 import './database';
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
