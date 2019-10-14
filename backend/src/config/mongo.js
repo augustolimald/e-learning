e5fd30741fde48d108dabadb008cea27e2efb86a
Augusto Lima <augustocesar827@gmail.com>
2019-10-14 14:54:43 -0300

    Adicionado campo usuario e senha

--- backend/src/config/mongo.js
+++ backend/src/config/mongo.js
@@ -2,12 +2,11 @@ const {
   MONGO_HOST: host,
   MONGO_PORT: port,
   MONGO_DB: db,
-  // MONGO_USER: user,
-  // MONGO_PASS: pass,
+  MONGO_USER: user,
+  MONGO_PASS: pass,
 } = process.env;
 
-// const url = `mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=admin`;
-const url = `mongodb://${host}:${port}/${db}?authSource=admin`;
+const url = `mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=admin`;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
