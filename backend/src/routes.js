57dedf15ed81d825457eb3402582e58200007ce0
Augusto Lima <augustocesar827@gmail.com>
2019-10-14 14:52:31 -0300

    Adicionado rotas de upload e certificado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,18 @@
-import { Router } from 'express';
-
-import authentication from './app/middlewares/auth';
+import multer from 'multer';
+import { resolve } from 'path';
+import express, { Router } from 'express';
+import multerConfig from './config/multer';
 import controllers from './app/controllers';
+import authentication from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/login', controllers.Login.login);
 routes.post('/users', controllers.User.store);
+routes.post('/login', controllers.Login.login);
+routes.post('/upload', upload.single('file'), controllers.Image.store);
+routes.use('/image', express.static(resolve(__dirname, '..', 'tmp', 'img')));
+routes.use('/certificate', express.static(resolve(__dirname, '..', 'tmp', 'certificate')));
 
 routes.use(authentication);
 
@@ -18,5 +24,6 @@ routes.get('/courses/:id', controllers.Course.show);
 routes.post('/courses/:id/subscribe', controllers.Subscription.store);
 routes.post('/courses/:id/progress', controllers.Subscription.progress);
 routes.post('/courses/:id/final_test', controllers.Subscription.submit);
+routes.get('/courses/:id/certificate', controllers.Subscription.certificate);
 
 export default routes;
